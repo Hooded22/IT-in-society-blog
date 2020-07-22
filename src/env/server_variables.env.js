2dc2ddb7ab9e343d703c0c1b60fc4280a7a77401
Przemyslaw Sipta <54626173+Hooded22@users.noreply.github.com>
2020-07-22 20:07:58 +0200

    Update server_variables.env.js

--- src/env/server_variables.env.js
+++ src/env/server_variables.env.js
@@ -1,5 +1,6 @@
-const SERVER_ADRESS = "https://it-in-society-cms.herokuapp.com"//"http://localhost:1337";
-const SERVER_URL = "https://it-in-society-cms.herokuapp.com"//"https://3efda454db1c.ngrok.io";
-const LOCAL_ADRESS = "http://localhost:8000"
+//Put your own
+const SERVER_ADRESS = "";
+const SERVER_URL = "";
+const LOCAL_ADRESS = ""
 
 export {SERVER_ADRESS, SERVER_URL, LOCAL_ADRESS};
