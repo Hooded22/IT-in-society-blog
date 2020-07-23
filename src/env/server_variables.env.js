56b6cb43ce0b0df4e9b4b049be00d86fedf65c2d
Przemek Sipta <przemek.sipta@gmail.com>
2020-07-23 18:20:31 +0200

    Update server variables

--- src/env/server_variables.env.js
+++ src/env/server_variables.env.js
@@ -1,6 +1,6 @@
 //Put your own
 const SERVER_ADRESS = "https://it-in-society-cms.herokuapp.com";
 const SERVER_URL = "https://it-in-society-cms.herokuapp.com";
-const LOCAL_ADRESS = "http://localhost:8000"
+const LOCAL_ADRESS = "https://5f19b68aad78c00289c2b24f--eager-poincare-38b9c0.netlify.app/"//"http://localhost:8000"
 
 export {SERVER_ADRESS, SERVER_URL, LOCAL_ADRESS};
